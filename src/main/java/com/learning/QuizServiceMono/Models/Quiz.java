07b750ca4bee557e9b8b602dbe57d5abdc0a420f
stwarHatLuffy <yadraj1803@gmail.com>
2024-03-08 10:37:06 -0500

    Second Commit main

--- src/main/java/com/learning/QuizServiceMono/Models/Quiz.java
+++ src/main/java/com/learning/QuizServiceMono/Models/Quiz.java
@@ -3,6 +3,7 @@ package com.learning.QuizServiceMono.Models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CascadeType;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class Quiz {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String title;
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     private List<Long> questionIds;
     private int score;
 }
--- src/main/java/com/learning/QuizServiceMono/Services/QuizService.java
+++ src/main/java/com/learning/QuizServiceMono/Services/QuizService.java
@@ -18,16 +18,18 @@ import java.util.Optional;
 public class QuizService {
     private final QuestionRepository questionRepository;
     private final QuizRepository quizRepository;
-    public QuizService(QuestionRepository questionRepository, QuizRepository quizRepository){
+
+    public QuizService(QuestionRepository questionRepository, QuizRepository quizRepository) {
         this.questionRepository = questionRepository;
         this.quizRepository = quizRepository;
     }
+
     public ResponseEntity<String> createQuiz(String title, String category, int totalQuestions) {
         Quiz quiz = new Quiz();
         quiz.setTitle(title);
         List<Question> questions = questionRepository.findQuestionsByCategory(category, totalQuestions);
         List<Long> questionIds = new ArrayList<>();
-        for(Question question : questions){
+        for (Question question : questions) {
             questionIds.add(question.getId());
         }
         quiz.setQuestionIds(questionIds);
@@ -36,50 +38,56 @@ public class QuizService {
     }
 
     public ResponseEntity<List<QuizQuestionDTO>> findQuizQuestions(Long id) throws NotFoundException {
-        Optional<Quiz> quizOptional= quizRepository.findById(id);
-        if(quizOptional.isEmpty()){
+        Optional<Quiz> quizOptional = quizRepository.findById(id);
+        if (quizOptional.isEmpty()) {
             throw new NotFoundException("Not found!");
         }
         Quiz quiz = quizOptional.get();
         List<Long> questionIds = quiz.getQuestionIds();
         List<Question> questions = questionRepository.findAllById(questionIds);
         List<QuizQuestionDTO> quizQuestionDTOS = new ArrayList<>();
-        for(Question question:questions){
+        for (Question question : questions) {
             quizQuestionDTOS.add(convertToQuizQuestionDto(question));
         }
         return new ResponseEntity<>(quizQuestionDTOS, HttpStatus.OK);
     }
-    public QuizQuestionDTO convertToQuizQuestionDto(Question question){
-        QuizQuestionDTO quizQuestionDTO = new QuizQuestionDTO();
-        quizQuestionDTO.setQuesTitle(question.getQuestion());
-        quizQuestionDTO.setCategory(question.getCategory());
-        quizQuestionDTO.setDifficulty(question.getDifficultyLevel());
-        quizQuestionDTO.setOption1(question.getOption1());
-        quizQuestionDTO.setOption2(question.getOption2());
-        quizQuestionDTO.setOption3(question.getOption3());
-        quizQuestionDTO.setOption4(question.getOption4());
-        return quizQuestionDTO;
+
+     public QuizQuestionDTO convertToQuizQuestionDto(Question question) {
+
+            QuizQuestionDTO quizQuestionDTO = new QuizQuestionDTO();
+            quizQuestionDTO.setQuesTitle(question.getQuestion());
+            quizQuestionDTO.setCategory(question.getCategory());
+            quizQuestionDTO.setDifficulty(question.getDifficultyLevel());
+            quizQuestionDTO.setOption1(question.getOption1());
+            quizQuestionDTO.setOption2(question.getOption2());
+            quizQuestionDTO.setOption3(question.getOption3());
+            quizQuestionDTO.setOption4(question.getOption4());
+
+            return quizQuestionDTO;
+
     }
+            public ResponseEntity<String> getScore (List < String > givenAnswers, Long quizId) throws NotFoundException
+            {
 
-
-    public ResponseEntity<String> getScore(List<String> givenAnswers, Long quizId) throws NotFoundException {
-
-        Optional<Quiz> quizOptional = quizRepository.findById(quizId);
-        if(quizOptional.isEmpty()){
-            throw new NotFoundException("Not Found");
-        }
-        Quiz quiz = quizOptional.get();
-
-        List<Long> questionIds = quiz.getQuestionIds();
-        List<Question> questions = questionRepository.findAllById(questionIds);
-        int right = 0;
-        for(Question question:questions){
-            for(String ans: givenAnswers){
-                if(question.getCorrectAns().equals(ans)){
-                    right++;
+                Optional<Quiz> quizOptional = quizRepository.findById(quizId);
+                if (quizOptional.isEmpty()) {
+                    throw new NotFoundException("Not Found");
                 }
+                Quiz quiz = quizOptional.get();
+
+                List<Long> questionIds = quiz.getQuestionIds();
+                List<Question> questions = questionRepository.findAllById(questionIds);
+                int right = 0;
+                for (Question question : questions) {
+                    for (String ans : givenAnswers) {
+                        if (question.getCorrectAns().equals(ans)) {
+                            right++;
+                        }
+                    }
+                }
+                quiz.setScore(right);
+                quizRepository.save(quiz);
+                return new ResponseEntity<>("Your score is " + right, HttpStatus.OK);
             }
-        }
-        return new ResponseEntity<>("Your score is "+right, HttpStatus.OK);
-    }
+
 }
